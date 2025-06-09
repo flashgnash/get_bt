48e94dd093bebfbcd8378936aa558da6d9a6748a
flashgnash <flashgnash@proton.me>
2025-06-09 19:13:26 +0100

    Update flake to allow use in nixos

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "Formatted BT device readout";
 
@@ -7,15 +6,15 @@
 
   };
 
-  outputs = { self, nixpkgs }: 
-  let 
-    system="x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    
-    devShells.${system}.default = 
-      pkgs.mkShell {
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           rustc
           cargo
@@ -23,38 +22,47 @@
           rust-analyzer
           clippy
           sqlite
-          
+
           pkgs.dbus.lib
         ];
 
         PKG_CONFIG_PATH = "${pkgs.dbus.dev}/lib/pkgconfig";
 
         RUST_BACKTRACE = "full";
-    };
-
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-      pname = "get_bt";
-      version = "0.0.1";
-      src = ./.; 
-      cargoBuildFlags = "-p get_bt";
-      
-      cargoLock = {
-        lockFile = ./Cargo.lock;
       };
 
-      nativeBuildInputs = with pkgs; [ pkg-config openssl.dev sqlite];
-      PKG_CONFIG_PATH = "${pkgs.dbus.dev}/lib/pkgconfig";
+      nixosModules.default =
+        { pkgs, ... }:
+        {
+          environment.systemPackages = [ self.packages.${pkgs.system}.default ];
+        };
 
-      buildPhase = ''
-        cargo build
-      '';
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "get_bt";
+        version = "0.0.1";
+        src = ./.;
+        cargoBuildFlags = "-p get_bt";
 
-      installPhase = ''
-        mkdir -p $out/bin
-        cp target/debug/get_bt $out/bin
-      '';
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
 
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          openssl.dev
+          sqlite
+        ];
+        PKG_CONFIG_PATH = "${pkgs.dbus.dev}/lib/pkgconfig";
+
+        buildPhase = ''
+          cargo build
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/debug/get_bt $out/bin
+        '';
+
+      };
     };
-  };
 }
-
